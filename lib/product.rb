166c091fdeae59c7e6f478b429883ba74b4096f9
Javelin89 <chris.beermann89@gmail.com>
2016-02-01 17:40:39 +0100

    Product section added

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,52 @@
 class Product
-	@@products = []
+  attr_reader :title, :price, :stock
 
-	def initialize(options={})
-		@@products << self
-	end
+  @@products = []
 
-	def self.all
-		@@products
-	end
+  def initialize(options={})
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_to_products
+  end
+
+  def self.all
+    @@products
+  end
+
+  def self.find_by_title(name)
+    #index = @@products.index(title).to_i
+    #@@products.fetch(index)
+    @@products.each do |product|
+      if product.title == name
+        return product
+      end
+    end
+  end
+
+  def self.in_stock
+    in_stock = []
+
+    @@products.each do |product|
+      unless product.stock == 0
+        in_stock_array << product
+      end
+    end
+
+    return in_stock
+  end
+
+  def in_stock?
+    @stock.to_i > 0 ? true : false
+  end
+
+  private
+
+  def add_to_products
+  	if !@@products.to_s.include?(self.title)
+  		@@products << self
+  	else 
+  		raise DuplicateProductError, "'#{@title}' already exists"
+  	end
+  end
 end
